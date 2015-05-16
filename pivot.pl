8e24071c81a81d0bd7b74624f693d051fca0045f
yamasita <yamasita.taisuke@bpsinc.jp>
2015-05-16 15:52:04 +0900

    update

--- pivot.pl
+++ pivot.pl
@@ -7,15 +7,18 @@ use Encode;
 use utf8;
 use File::Temp qw/tempdir/;
 binmode STDOUT, ":utf8";
+use Time::Piece;
+use Time::Seconds;
 
 my $p;
+my $previous_day = &latest_data;
 
 # 前日分の高値,安値,終値を取得
 {
     my $tempdir = tempdir(CLEANUP => 1);
     local $ENV{LANG} = "C";
     chdir $tempdir;
-    system("curl -fs http://www.tfx.co.jp/kawase/document/PRT-010-CSV-003-\$(date +%Y%m%d -d '1 days ago').CSV > input.csv") and die $!;
+    system("curl -fs http://www.tfx.co.jp/kawase/document/PRT-010-CSV-003-$previous_day.CSV > input.csv") and die $!;
     open(my $fh, "<:encoding(Shift-JIS)","input.csv") or die $!;
     while(<$fh>){
         if (/米ドル・日本円取引所為替証拠金取引/){
@@ -27,16 +30,36 @@ my $p;
 }
 chdir "/tmp";
 
-my ($title, $val);
+my ($line, $val);
+format STDOUT_TOP =
+@<<<年@<月@<日(@)データ
+substr($previous_day,0,4),substr($previous_day,4,2),substr($previous_day,6,2),&wday($previous_day)
+
+.
 format STDOUT =
 @<: @<<<<<<<
-$title,$val
+$line,$val
 .
 
 for (qw(H R2 R1 P S1 S2 L)){
-    $title = $_;
+    $line = $_;
     $val = $p->{$_};
     write;
+}
+
+sub wday {
+    my $yyyymmdd = shift;
+    my @wdays = ("日","月","火","水","木","金","土");
+    my $t = Time::Piece->strptime($yyyymmdd,"%Y%m%d");
+    return $wdays[$t->_wday];
+}
+sub latest_data {
+    open (my $web,'-|:encoding(Shift-JIS)',"curl -fs http://www.tfx.co.jp/cgi-bin/statistics_forex_j.cgi") or die $!;
+    while (<$web>){
+        if ($_ =~ m#/kawase/document/PRT-\d{3}-CSV-\d{3}-(\d{8}).CSV>.*CSV形式#){
+            return $1;
+        }
+    }
 }
 
 package Pivot;
@@ -45,16 +68,16 @@ sub new {
     my $self = {
         @_,
     };
-    $self->{P}  = ($self->{'High'} + $self->{'Low'} + $self->{'Close'})/3;
+    $self->{P}  = ($self->{'High'} + $self->{'Low'}   + $self->{'Close'})/3;
 
-    $self->{S1} = $self->{'P'}  - ($self->{'High'} - $self->{'P'});
-    $self->{R1} = $self->{'P'}  + ($self->{'P'}    - $self->{'Low'});
+    $self->{S1} = $self->{'P'}     - ($self->{'High'} - $self->{'P'});
+    $self->{R1} = $self->{'P'}     + ($self->{'P'}    - $self->{'Low'});
 
-    $self->{S2} = $self->{'P'}  - ($self->{'High'} - $self->{'Low'});
-    $self->{R2} = $self->{'P'}  + ($self->{'High'} - $self->{'Low'});
+    $self->{S2} = $self->{'P'}     - ($self->{'High'} - $self->{'Low'});
+    $self->{R2} = $self->{'P'}     + ($self->{'High'} - $self->{'Low'});
 
-    $self->{L}  = $self->{'S1'} - ($self->{'High'} - $self->{'Low'});
-    $self->{H}  = $self->{'R1'} + ($self->{'High'} - $self->{'Low'});
+    $self->{L}  = $self->{'S1'}    - ($self->{'High'} - $self->{'Low'});
+    $self->{H}  = $self->{'R1'}    + ($self->{'High'} - $self->{'Low'});
 
     my $keta = sub {
         my $source_ref = shift;
